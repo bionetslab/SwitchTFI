6314e51be13a9628d914f105c3f36f949353d087
Paul Martini <paul.martini@web.de>
2025-07-30 18:21:16 +0200

    update example.py

--- example.py
+++ example.py
@@ -1,52 +1,98 @@
 
-from switchtfi.data import erythrocytes, erythrocytes_grn
-from switchtfi.fit import fit_model
-from switchtfi.weight_fitting import calculate_weights
-from switchtfi.pvalue_calculation import compute_westfall_young_adjusted_pvalues, remove_insignificant_edges
-from switchtfi.tf_ranking import rank_tfs
-
 
 def main():
-    # Load the preprocessed scRNA-seq data
-    erydata = erythrocytes()
 
-    # Load the GRN (previously inferred with the Scenic method)
-    erygrn = erythrocytes_grn()
+    import numpy as np
+    import matplotlib.pyplot as plt
 
-    # ### Perform SwitchTFI analyses
+    from switchtfi import (
+        erythrocytes, preendocrine_beta, preendocrine_alpha,
+        erythrocytes_grn, preendocrine_beta_grn, preendocrine_alpha_grn,
+        fit_model,
+        calculate_weights,
+        compute_corrected_pvalues,
+        remove_insignificant_edges,
+        rank_tfs,
+        plot_regulon
+    )
+
+    tissue = 'alpha'  # 'ery', 'beta', 'alpha'
+
+    # Load the preprocessed scRNA-seq data and GRN (previously inferred with the Scenic method)
+    if tissue == 'ery':
+        data = erythrocytes()
+        grn = erythrocytes_grn()
+        prog_off_annotations_key = 'prog_off'
+    elif tissue == 'beta':
+        data = preendocrine_beta()
+        grn = preendocrine_beta_grn()
+        prog_off_annotations_key = 'clusters'
+    else:
+        data = preendocrine_alpha()
+        grn = preendocrine_alpha_grn()
+        prog_off_annotations_key = 'clusters'
+
+    # ### Perform SwitchTFI analyses ### #
     # - Compute weights and empirical corrected p-values for each edge in the input GRN
     # - Prune the input GRN: remove edge if p-value > FWER-threshold  ==> transition GRN
     # - Rank transcription factors according to centrality in transition GRN
 
     # Run fit function => transition GRN and ranked TFs
-    ery_transitiongrn, ery_ranked_tfs = fit_model(adata=erydata,
-                                                  grn=erygrn,
-                                                  clustering_obs_key='prog_off',
-                                                  verbosity=1)
-    print(f'# ### Transition GRN erythrocytes differentiation:\n{ery_transitiongrn}')
-    print(f'# ### Transition driver TFs erythrocytes differentiation:\n{ery_ranked_tfs.head(10)}')
+    np.random.seed(42)
+    transition_grn, ranked_tfs = fit_model(
+        adata=data,
+        grn=grn,
+        layer_key='magic_imputed',
+        n_permutations=1000,
+        clustering_obs_key=prog_off_annotations_key,
+    )
 
-    # The steps of SwitchTFI's analyses can be performed individually as well
-    # Note that now the weighted outdegree is used for ranking the TFs in the transition GRN
-    erygrn_weighted = calculate_weights(adata=erydata,
-                                        grn=erygrn,
-                                        layer_key='magic_imputed',
-                                        clustering_obs_key='prog_off')
+    print(f'# ### Transition GRN:\n{transition_grn}')
+    print(f'# ### Transition driver TFs (ranked by PageRank):\n{ranked_tfs}')
 
-    erygrn_weighted_with_pvals = compute_westfall_young_adjusted_pvalues(adata=erydata,
-                                                                         grn=erygrn_weighted,
-                                                                         n_permutations=1000,
-                                                                         clustering_obs_key='prog_off')
+    # ### The steps of SwitchTFI's analyses can be performed individually as well
+    # Note: Now the weighted outdegree is used for ranking the TFs in the transition GRN
+    np.random.seed(42)
+    grn_weighted = calculate_weights(
+        adata=data,
+        grn=grn,
+        layer_key='magic_imputed',
+        clustering_obs_key=prog_off_annotations_key
+    )
 
-    ery_transition_grn = remove_insignificant_edges(grn=erygrn_weighted_with_pvals, alpha=0.05, inplace=False)
+    grn_weighted_with_pvals = compute_corrected_pvalues(
+        adata=data,
+        grn=grn_weighted,
+        n_permutations=1000,
+        clustering_obs_key=prog_off_annotations_key,
+    )
 
-    ery_ranked_tfs = rank_tfs(grn=ery_transition_grn,
-                              reverse=False,
-                              centrality_measure='out_degree',
-                              weight_key='score')
+    transition_grn = remove_insignificant_edges(grn=grn_weighted_with_pvals, alpha=0.05, inplace=False)
 
-    print(f'# ### Transition GRN erythrocytes differentiation:\n{ery_transitiongrn}')
-    print(f'# ### Transition driver TFs erythrocytes differentiation:\n{ery_ranked_tfs.head(10)}')
+    ranked_tfs = rank_tfs(
+        grn=transition_grn,
+        reverse=False,
+        centrality_measure='out_degree',
+        weight_key='score'
+    )
+
+    # Subset the GRN to the relevant columns
+    transition_grn = transition_grn[['TF', 'target', 'weight', 'pvals_wy']]
+
+    print(f'# ### Transition GRN:\n{transition_grn}')
+    print(f'# ### Transition driver TFs (ranked by weighted outdegree):\n{ranked_tfs}')
+
+    # Visualize the regulon of the top ranked TF
+    top_tf = ranked_tfs.loc[0, 'gene']
+
+    plot_regulon(
+        grn=transition_grn,
+        tf=top_tf,
+        sort_by='score',
+        top_k=10
+    )
+
+    plt.show()
 
 
 if __name__ == '__main__':
